4859c1b0284a9e4750c0be466583b2b428d85ec9
MaharshiPatel <maharshi11patel@gmail.com>
2021-04-21 19:32:29 -0700

    replaced dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,3 @@
-FROM ubuntu:18.04
-ADD . /
-RUN chmod +x /install.sh
+  FROM mpatel.jfrog.io/su113-docker/alpine:3.11.5
 
-RUN /install.sh && rm -rf /tmp && mkdir /tmp && chmod 1777 /tmp
-
-ENV BASH_ENV "/etc/drydock/.env"
-
+CMD printf "\nCONGRATULATIONS!!!\n\nYou have just set up your first Docker repository with the new JFrog Platform!\n\n
--- install.sh
+++ install.sh
@@ -19,32 +19,29 @@ openssh-client \
 ftp \
 gettext \
 smbclient \
-mercurial
+mercurial \
+software-properties-common \
+git
+
+apt-add-repository universe
+apt-get update
 
 echo "================= Installing Python packages =================="
 apt-get install -q -y \
-python-pip \
-python2.7-dev
+python3-dev \
+python3-pip
 
-pip install -q virtualenv==16.7.0
-pip install -q pyOpenSSL==19.1.0
+pip3 install -q virtualenv==16.7.0
+pip3 install -q pyOpenSSL==19.1.0
 
-export JQ_VERSION=1.5*
+export JQ_VERSION=1.*
 echo "================= Adding JQ $JQ_VERSION ========================="
 apt-get install -y -q jq="$JQ_VERSION"
 
 echo "================= Installing CLIs packages ======================"
+wget -qO - https://releases.jfrog.io/artifactory/api/gpg/key/public | sudo apt-key add -;
+echo "deb https://releases.jfrog.io/artifactory/jfrog-debs xenial contrib" | sudo tee -a /etc/apt/sources.list;
+apt-get update;
+apt-get install -y jfrog-cli;
 
-export GIT_VERSION=1:2.*
-echo "================= Installing Git $GIT_VERSION ===================="
-add-apt-repository ppa:git-core/ppa -y
-apt-get update -qq
-apt-get install -y -q git="$GIT_VERSION"
-
-
-JFROG_VERSION=1.33.2
-echo "================= Adding jfrog-cli $JFROG_VERSION  ================"
-wget -nv https://api.bintray.com/content/jfrog/jfrog-cli-go/"$JFROG_VERSION"/jfrog-cli-linux-amd64/jfrog?bt_package=jfrog-cli-linux-amd64 -O jfrog
-sudo chmod +x jfrog
-mv jfrog /usr/bin/jfrog
 
